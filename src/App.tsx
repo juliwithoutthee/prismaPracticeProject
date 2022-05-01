18b9af03fcb8c93eb589900183697a4e25a93da4
juliwithoutthee <jnboucree@gmail.com>
2022-05-01 22:58:07 +0900

    Updates App

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
       <Navbar></Navbar>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello!</h1>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
